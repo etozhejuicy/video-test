fc4fb38304424d1c2e4e91af8d580201eca7feec
serj <zenitovic@gmail.com>
2024-10-24 22:39:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const video = document.getElementById("demo");
 
 video.pause();
-setTimeout(() => {
+
+setTimeout(function () {
   video.setAttribute("muted", true);
   video.setAttribute("loop", true);
   video.setAttribute("playinline", true);
   video.setAttribute("autoplay", true);
   video.removeAttribute("controls");
-  video.dispatchEvent(new Event("click"));
   video.play();
 }, 1000);
