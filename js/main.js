1a09deb3e6d43474b5cac24499dd33c390960433
serj <zenitovic@gmail.com>
2024-10-25 23:23:43 +0300

    update video

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
-const video = document.getElementById("demo");
+const video = document.querySelector(".bg-video");
+const videoDesktop = document.querySelector(".bg-video-desktop");
 
-video.pause();
+const startThisVideo = () => {
+  if (window.innerWidth > 1024) {
+    videoDesktop.pause();
+    video.pause();
 
-setTimeout(function () {
-  video.setAttribute("muted", true);
-  video.setAttribute("loop", true);
-  video.setAttribute("playsinline", true);
-  video.setAttribute("autoplay", true);
-  video.removeAttribute("controls");
-  video.dispatchEvent(new Event("click"));
-  video.play();
-}, 1000);
+    setTimeout(function () {
+      video.setAttribute("muted", true);
+      video.setAttribute("loop", true);
+      video.setAttribute("playsinline", true);
+      video.setAttribute("autoplay", true);
+      video.removeAttribute("controls");
+      video.dispatchEvent(new Event("click"));
+      video.play();
+    }, 1000);
+  } else {
+    video.pause();
+    videoDesktop.pause();
+
+    setTimeout(function () {
+      videoDesktop.setAttribute("muted", true);
+      videoDesktop.setAttribute("loop", true);
+      videoDesktop.setAttribute("playsinline", true);
+      videoDesktop.setAttribute("autoplay", true);
+      videoDesktop.removeAttribute("controls");
+      videoDesktop.dispatchEvent(new Event("click"));
+      videoDesktop.play();
+    }, 1000);
+  }
+};
+
+window.addEventListener("resize", () => {
+  startThisVideo();
+});
